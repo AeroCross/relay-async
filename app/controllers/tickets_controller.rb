6aa15444cb36a30b649dea437bfd4b996a320d30
Mario Cuba <mario@mariocuba.net>
2015-06-10 21:20:39 +0100

    Possible that tickets controller uses all relations.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -64,7 +64,7 @@ class TicketsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_ticket
-      @ticket = Ticket.find(params[:id])
+      @ticket = Ticket.includes(:user, :messages).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
