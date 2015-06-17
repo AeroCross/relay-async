2ce6c06bedf50241ee55a8c9ff46c22db2c1b4bc
Mario Cuba <mario@mariocuba.net>
2015-06-17 21:30:10 +0100

    Show proper alerts in tickets.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -32,7 +32,7 @@ class TicketsController < ApplicationController
 
     respond_to do |format|
       if @ticket.save
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully created.' }
+        format.html { redirect_to @ticket, notice: 'Ticket was successfully created.', flash: {type: 'success'} }
         format.json { render :show, status: :created, location: @ticket }
       else
         format.html { render :new }
@@ -46,7 +46,7 @@ class TicketsController < ApplicationController
   def update
     respond_to do |format|
       if @ticket.update(ticket_params)
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.' }
+        format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.', flash: {type: 'info'} }
         format.json { render :show, status: :ok, location: @ticket }
       else
         format.html { render :edit }
@@ -60,7 +60,7 @@ class TicketsController < ApplicationController
   def destroy
     @ticket.destroy
     respond_to do |format|
-      format.html { redirect_to tickets_url, notice: 'Ticket was successfully destroyed.' }
+      format.html { redirect_to tickets_url, notice: 'Ticket was successfully destroyed.', flash: {type: 'warning'} }
       format.json { head :no_content }
     end
   end
