99bb088fabdfc64880ca3f8cdf5f381db2a30eb0
Mario Cuba <mario@mariocuba.net>
2015-06-25 22:07:56 +0100

    Add auth code to email.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -6,6 +6,9 @@ class TicketMailer < ApplicationMailer
     @message = message
     @ticket_url = ticket_url id: @ticket.id
 
+    # to model
+    @auth = Ticket.find(ticket).auth_client
+
     # @TODO: this should be a global default or something
     @chat_url = 'http://localhost:8080/chat/' + @ticket.id.to_s
     mail to: @user.email, subject: "Response about #{@ticket.id}: #{@ticket.subject}"
