4c327b7a2c01efe49a807b4281d573b6f5d5574e
Mario Cuba <mario@mariocuba.net>
2015-06-29 21:31:19 +0100

    Can change status of ticket in show.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -81,6 +81,6 @@ class TicketsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ticket_params
-      params.require(:ticket).permit(:user_id, :subject, :content)
+      params.require(:ticket).permit(:user_id, :subject, :content, :status)
     end
 end
