b2a63d2bc6d672284bb70548aaef6a4857a216c6
Mario Cuba <mario@mariocuba.net>
2015-07-14 21:58:17 +0100

    Smaller chat messages in show tickets. Also, show who's an admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # @TODO: maybe this can be implemented only in public-facing controllers?
   protect_from_forgery with: :null_session
 
   private
