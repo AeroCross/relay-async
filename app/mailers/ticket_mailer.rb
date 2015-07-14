9a499b9ad587a7c2cc47dd3cb43c6bb3f226808a
Mario Cuba <mario@mariocuba.net>
2015-07-14 21:35:48 +0100

    Properly generate URL in mail messages.

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,16 +1,19 @@
 class TicketMailer < ApplicationMailer
+  # @TODO: change when in production - maybe a config variable or somethinf
   default from: 'notifications@example.com'
+
+  # send email to the requester when a new response has been sent
   def chat_invite(user, ticket, message)
-    @user = user
-    @ticket = ticket
-    @message = message
-    @ticket_url = ticket_url id: @ticket.id
+    # set up all variables to be used within the message
+    @user         = user
+    @ticket       = ticket
+    @message      = message
+    @auth         = Ticket.find(ticket).auth_client
+    @sign_up_url  = access_sign_up_url
+    @ticket_url   = ticket_url id: @ticket.id
+    @chat_url     = Rails.configuration.sync.root + '/chat/' + @ticket.id.to_s + '?email=' + @user.email + '&auth=' + @auth
 
-    # to model
-    @auth = Ticket.find(ticket).auth_client
-
-    # @TODO: this should be a global default or something
-    @chat_url = 'http://localhost:8080/chat/' + @ticket.id.to_s
+    # send the email
     mail to: @user.email, subject: "Response about #{@ticket.id}: #{@ticket.subject}"
   end
 end
